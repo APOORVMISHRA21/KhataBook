9cf590d913f3b7f91770b7bd4591772aef41dd92
prashantprem <premifsb@gmail.com>
2021-11-20 17:19:54 +0530

    Login Added

--- app/src/main/java/com/example/khatabook/SplashActivity.java
+++ app/src/main/java/com/example/khatabook/SplashActivity.java
@@ -15,7 +15,8 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         new Handler(Looper.getMainLooper()).postDelayed((Runnable) () -> {
-            startActivity(new Intent(SplashActivity.this, CaraouselActivity.class));
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
         }, 3000);
     }
 }
