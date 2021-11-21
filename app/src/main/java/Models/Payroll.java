23652ce32a08c95495ad37054aa87569c1d8d6a1
prashantprem <premifsb@gmail.com>
2021-11-21 14:58:26 +0530

    Payroll added

---
+++ app/src/main/java/Models/Payroll.java
@@ -0,0 +1,32 @@
+package Models;
+
+public class Payroll {
+
+    private int Employees;
+    private int Salary;
+    private String Department;
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
+
+    public int getEmployees() {
+        return Employees;
+    }
+
+    public void setEmployees(int employees) {
+        Employees = employees;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(int salary) {
+        Salary = salary;
+    }
+}
--- app/src/main/java/com/example/khatabook/SplashActivity.java
+++ app/src/main/java/com/example/khatabook/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         new Handler(Looper.getMainLooper()).postDelayed((Runnable) () -> {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            startActivity(new Intent(SplashActivity.this, PayrollActivity.class));
             finish();
         }, 3000);
     }
